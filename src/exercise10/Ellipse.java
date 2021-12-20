52d2b530381bd4236120f3847c3ef82e218e1c0b
martonikon <>
2021-12-20 22:57:47 +0000

    Some message

--- src/exercise10/Ellipse.java
+++ src/exercise10/Ellipse.java
@@ -1,70 +1,56 @@
 package exercise10;
 
-public class Ellipse {
-    private Point startPoint;
-    private double a;
-    private double b;
+import java.lang.Math;
+
+public class Ellipse extends Figure {
+
 
     public Ellipse() {
-        startPoint = new Point(0, 0);
-        a = 1;
-        b = 1;
+       super(new Point(0,0), 1,1);
     }
 
-    public Ellipse(Point startPoint, double a, double b) {
-        this.startPoint = new Point(startPoint);
-        this.a = a;
-        this.b = b;
+    public Ellipse(Point startPoint, double side1, double side2) {
+        super(new Point(startPoint), side1, side2);
     }
 
     public Ellipse(Ellipse otherEllipse) {
-        startPoint = new Point(otherEllipse.startPoint);
-        a = otherEllipse.a;
-        b = otherEllipse.b;
+        super(new Point(otherEllipse.startPoint), otherEllipse.side1, otherEllipse.side2);
     }
 
-    public boolean isValid() {
-        return a > 0 && b > 0;
-    }
 
-    public void initialize() {
-        do {
-            System.out.println("Start point: ");
-            startPoint.initialize();
-            System.out.print("Enter a: ");
-            a = Utils.INPUT.nextDouble();
-            System.out.print("Enter b: ");
-            b = Utils.INPUT.nextDouble();
-        } while (!isValid());
-    }
-
+    @Override
     public double calculatePerimeter() {
-        return Math.PI * (3.0 * (a + b) - Math.sqrt((3.0 * a + b) * (a + 3.0 * b)));
+        return Math.PI * (3.0 * (side1 + side2) - Math.sqrt((3.0 * side1 + side2) * (side1 + 3.0 * side2)));
     }
 
+    @Override
     public double calculateArea() {
-        return Math.PI * a * b;
+        return Math.PI * side1 * side2;
     }
 
+    @Override
     public String getType() {
-        return (a == b) ? "Circle" : "Ellipse";
+        return (side1 == side2) ? "Circle" : "Ellipse";
     }
 
-    public String toString() {
-        return String.format("%s-[%s, %s], %s, P=%s, A=%s", startPoint, a, b, getType(), calculatePerimeter(), calculateArea());
-    }
 
-    public boolean equal(Ellipse otherEllipse) {
-        boolean sameA = Utils.equals(a, otherEllipse.a);
-        boolean sameB = Utils.equals(b, otherEllipse.b);
-        boolean sameAReversed = Utils.equals(a, otherEllipse.b);
-        boolean sameBReversed = Utils.equals(b, otherEllipse.a);
+    @Override
+    public boolean equal(Figure otherFigure) {
+        boolean sameA = Utils.equals(side1, otherFigure.side1);
+        boolean sameB = Utils.equals(side2, otherFigure.side2);
+        boolean sameAReversed = Utils.equals(side1, otherFigure.side2);
+        boolean sameBReversed = Utils.equals(side2, otherFigure.side1);
 
         return (sameA && sameB) || (sameAReversed && sameBReversed);
     }
 
     public boolean containsClick(Point click) {
-        // Check if click point is inside the rectangle
+        double xce = startPoint.x + side1/2;
+        double yce = startPoint.y + side2/2;
+
+        if ((Math.pow(click.x - xce, 2)/Math.pow(side1,2)) + (Math.pow(click.y - yce, 2)/Math.pow(side2,2)) <= 1 ) {
+            return true;
+        }
         return false;
     }
 }
--- src/exercise10/Rectangle.java
+++ src/exercise10/Rectangle.java
@@ -4,30 +4,24 @@ public class Rectangle extends Figure {
     public Rectangle() {
         super(new Point(0, 0), 1, 1);
     }
-
     public Rectangle(Point startPoint, double height, double width) {
         super(startPoint, height, width);
     }
-
     public Rectangle(Rectangle otherRectangle) {
         super(otherRectangle.startPoint, otherRectangle.side1, otherRectangle.side2);
     }
-
     @Override
     public double calculatePerimeter() {
         return side1 * 2 + side2 * 2;
     }
-
     @Override
     public double calculateArea() {
         return side1 * side2;
     }
-
     @Override
     public String getType() {
         return (side1 == side2) ? "Square" : "Rectangle";
     }
-
     @Override
     public boolean equal(Figure otherFigure) {
         if (otherFigure instanceof Rectangle) {
@@ -36,10 +30,11 @@ public class Rectangle extends Figure {
             return false;
         }
     }
-
     @Override
     public boolean containsClick(Point click) {
-        // Check if click point is inside the rectangle
+        if (click.x >= startPoint.x && click.x <= (startPoint.x + side2) && click.y >= startPoint.y && click.y <= (startPoint.y + side1)) {
+            return true;
+        }
         return false;
     }
 }
